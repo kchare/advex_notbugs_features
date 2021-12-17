d4c87d930ab508bee0630a9e1b4f9649ab2c3597
Sivananda Rajananda <>
2021-12-17 12:43:58 -0500

    updated functions

--- helpers/data_processing.py
+++ helpers/data_processing.py
@@ -4,12 +4,15 @@ Defines helper functions for data processing
 
 import tensorflow as tf
 
-def normalize_img(img, label):
-    """Normalizes image to [0,1] for better model training"""
-    return img / 255, label
+def train_preprocess(img, label):
+  img /= 255
+  img = tf.cast(img, tf.float32)
+  img = tf.image.resize_with_pad(img, 32+4, 32+4)
+  img = tf.image.random_crop(img, size=[32, 32, 3])
+  img = tf.image.stateless_random_flip_left_right(img, (15, 13))
+  return img, label
 
-def make_tf_data(raw_ds):
-    """Applies image normalization to raw TF Data structure"""
-    ds = raw_ds.map(normalize_img)
-    return ds
-
+def test_preprocess(img, label):
+  img = tf.cast(img, tf.float32)
+  img /= 255
+  return img, label
--- helpers/pgd.py
+++ helpers/pgd.py
@@ -13,13 +13,16 @@ import tensorflow as tf
 def onestep_pgd_linf(model, X, y, epsilon, alpha, delta):
     with tf.GradientTape() as tape:
         tape.watch(delta)
-        loss = tf.keras.losses.SparseCategoricalCrossentropy()(y, model(X + delta))
+        loss = tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True,
+            reduction=tf.keras.losses.Reduction.NONE
+        )(y, model(X + delta))
 
-    delta = tf.clip_by_value(delta + alpha*tf.sign(tape.gradient(loss, delta)), -epsilon, epsilon)
-    
+    delta = tf.clip_by_value(delta + alpha*tf.sign(tape.gradient(loss, delta)), X-epsilon, X+epsilon)
+
     return delta
 
-# Full run – import this 
+# Full run – import this
 def pgd_linf(model, X, y, epsilon, alpha, num_iter):
     """ Construct FGSM adversarial examples on the examples X"""
     delta = tf.zeros_like(X)
@@ -35,28 +38,58 @@ def pgd_linf(model, X, y, epsilon, alpha, num_iter):
 # Helper
 def norm(Z):
     """Compute norms over all but the first dimension"""
-    return tf.norm(Z[0,:,:,:])
+    return tf.norm(tf.reshape(Z, (Z.shape[0], -1)), axis=1)
 
 # PGD L2 for Robustifying #
 def single_pgd_step_robust(model, X, y, alpha, delta):
     with tf.GradientTape() as tape:
         tape.watch(delta)
-        loss = tf.keras.losses.MeanSquaredError()(y, model(X + delta)) # comparing to robust model representation layer
-        
+        loss = tf.keras.losses.MeanSquaredError(
+            reduction=tf.keras.losses.Reduction.NONE
+        )(y, model(X + delta)) # comparing to robust model representation layer
+
     grad = tape.gradient(loss, delta)
-    delta -= alpha*grad / norm(grad) # normalized gradient step
+    normgrad = tf.reshape(norm(grad), (-1, 1, 1, 1))
+    delta -= alpha*grad / (normgrad + 1e-10) # normalized gradient step
     delta = tf.math.minimum(tf.math.maximum(delta, -X), 1-X) # clip X+delta to [0,1]
-    
     return delta, loss
 
-@tf.function
 def pgd_l2_robust(model, X, y, alpha, num_iter, epsilon=0, example=False):
     delta = tf.zeros_like(X)
     loss = 0
+    fn = tf.function(single_pgd_step_robust)
     for t in range(num_iter):
-        delta, loss = single_pgd_step_robust(model, X, y, alpha, delta)
-
+      delta, loss = fn(model, X, y, alpha, delta)
     # Prints out loss to evaluate if it's actually learning (currently broken)
+    if example:
+        print(f'{num_iter} iterations, final MSE {loss}')
+    return delta
+
+# PGD L2 for Non-Robustifying #
+def single_pgd_step_nonrobust(model, X, y, alpha, epsilon, delta):
+    with tf.GradientTape() as tape:
+        tape.watch(delta)
+        loss = tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True,
+            reduction=tf.keras.losses.Reduction.NONE # Use no aggregation - will give gradient separtely for each ex.
+            )(y, model(X + delta)) # comparing to label for original data point
+    grad = tape.gradient(loss, delta) #tape.gradient(loss, delta)
+
+    # equivalent to delta += alpha*grad / norm(grad), just for batching
+    normgrad = tf.reshape(norm(grad), (-1, 1, 1, 1))
+    # changed from plus to minus b/c trying to minimize with non-robust
+    z = delta - alpha * (grad / (normgrad + 1e-10))
+    normz = tf.reshape(norm(z), (-1, 1, 1, 1))
+    delta = epsilon * z / (tf.math.maximum(normz, epsilon) + 1e-10)
+    return delta, loss
+
+def pgd_l2_nonrobust(model, X, y, alpha, num_iter, epsilon=0, example=False):
+    fn = tf.function(single_pgd_step_nonrobust)
+    delta = tf.zeros_like(X)
+    loss = 0
+    for t in range(num_iter):
+        delta, loss = fn(model, X, y, alpha, epsilon, delta)
+
     if example:
         print(f'{num_iter} iterations, final MSE {loss}')
     return delta
@@ -65,23 +98,26 @@ def pgd_l2_robust(model, X, y, alpha, num_iter, epsilon=0, example=False):
 def single_pgd_step_adv(model, X, y, alpha, epsilon, delta):
     with tf.GradientTape() as tape:
         tape.watch(delta)
-        loss = tf.keras.losses.SparseCategoricalCrossentropy()(y, model(X + delta)) # comparing to label for original data point
-
+        loss = tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True,
+            reduction=tf.keras.losses.Reduction.NONE # Use no aggregation - will give gradient separtely for each ex.
+            )(y, model(X + delta)) # comparing to label for original data point
     grad = tape.gradient(loss, delta)
-    delta += alpha*grad / norm(grad) # normalized gradient step
-    delta = tf.math.minimum(tf.math.maximum(delta, -X), 1-X) # clip X+delta to [0,1]
-    # Distinguishes _adv from _robust – bounds the change to the image
-    delta *= epsilon / tf.clip_by_value(norm(delta), epsilon, np.inf)
-    
+
+    normgrad = tf.reshape(norm(grad), (-1, 1, 1, 1))
+    z = delta + alpha * (grad / (normgrad + 1e-10))
+
+    normz = tf.reshape(norm(z), (-1, 1, 1, 1))
+    delta = epsilon * z / (tf.math.maximum(normz, epsilon) + 1e-10)
     return delta, loss
 
-@tf.function
 def pgd_l2_adv(model, X, y, alpha, num_iter, epsilon=0, example=False):
+    fn = tf.function(single_pgd_step_adv)
     delta = tf.zeros_like(X)
     loss = 0
     for t in range(num_iter):
-        delta, loss = single_pgd_step_adv(model, X, y, alpha, epsilon, delta)
-        
+        delta, loss = fn(model, X, y, alpha, epsilon, delta)
+
     if example:
         print(f'{num_iter} iterations, final MSE {loss}')
     return delta
